20d72269d730785f9743fb52285c7c8a3f4ccaf9
Damon <strassburger.michelle06@gmail.com>
2026-01-31 11:20:49 +0100

    Movement

---
+++ Assets/Scenes/PlayerMovement.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    public float move_speed;
+
+    public float ground_drag;
+
+    public float jump_force;
+    public float jump_cooldown;
+    public float air_multiplier;
+    bool ready_to_jump;
+
+    [Header("Keybinds")]
+    public KeyCode jump_key = KeyCode.Space;
+
+    [Header("Ground Check")]
+    public float player_height;
+    public LayerMask what_is_ground;
+    bool grounded;
+
+    public Transform orientation;
+
+    float horizontal_input;
+    float vertical_input;
+
+    Vector3 movement_direction;
+
+    Rigidbody rigid_body;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rigid_body = GetComponent<Rigidbody>();
+        rigid_body.freezeRotation = true;
+        ready_to_jump = true;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // ground check
+        grounded = Physics.Raycast(transform.position, Vector3.down, player_height * 0.5f + 0.2f, what_is_ground);
+        
+        MyInput();
+        SpeedControl();
+
+        // handle drag
+        if (grounded)
+            rigid_body.drag = ground_drag;
+        else
+            rigid_body.drag = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MyInput()
+    {
+        horizontal_input = Input.GetAxisRaw("Horizontal");
+        vertical_input = Input.GetAxisRaw("Vertical");
+
+        // when to jump
+        if (Input.GetKey(jump_key) && ready_to_jump && grounded)
+        {
+            ready_to_jump = false;
+            Jump();
+
+            Invoke(nameof(ResetJump), jump_cooldown);
+        }
+    }
+
+    private void MovePlayer()
+    {
+        // calculate movement direction
+        movement_direction = orientation.forward * vertical_input + orientation.right * horizontal_input;
+
+        // on ground
+        if (grounded)
+            rigid_body.AddForce(movement_direction.normalized * move_speed * 10f, ForceMode.Force);
+        // in air
+        else if (!grounded)
+            rigid_body.AddForce(movement_direction.normalized * move_speed * 10f * air_multiplier, ForceMode.Force);
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flat_velocity = new Vector3(rigid_body.velocity.x, 0f, rigid_body.velocity.z);
+
+        // limit velocity if needed
+        if (flat_velocity.magnitude > move_speed)
+        {
+            Vector3 limited_velocity = flat_velocity.normalized * move_speed;
+            rigid_body.velocity = new Vector3(limited_velocity.x, rigid_body.velocity.y, limited_velocity.z);
+        }
+    }
+
+    private void Jump()
+    {
+        // reset y velocity
+        rigid_body.velocity = new Vector3(rigid_body.velocity.x, 0f, rigid_body.velocity.z);
+
+        rigid_body.AddForce(transform.up * jump_force, ForceMode.Impulse);
+    }
+    private void ResetJump()
+    {
+        ready_to_jump = true;
+    }
+}
