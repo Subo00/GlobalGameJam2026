d588fbacc54b6002d20b8d724679cde2e9a5bfde
Ernest Subotić <gamedevsubo@gmail.com>
2026-01-31 17:47:05 +0100

    add Interactables and PlayerHUD

---
+++ Assets/Scripts/Interactables/Interactable.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public abstract class Interactable : MonoBehaviour, IMyUpdate
+{
+    protected Transform dropPoint = null;
+    protected PlayerMovement interactingPlayer = null;
+
+    private PlayerHUD playerHUD = null;
+
+    public abstract void OnInteract(PlayerMovement player);
+    protected abstract void OnUpdate();
+    void IMyUpdate.MyUpdate()
+    {
+        OnUpdate();
+    }
+    protected void CommonLogic()
+    {
+        playerHUD.ShowInteractionOnObject(dropPoint.position);
+    }
+
+    protected virtual void Start()
+    {
+        //Make sure that the gameObject dropPoint is a child of the GO
+        //that this script is attached to
+        Transform[] temp = gameObject.GetComponentsInChildren<Transform>();
+        dropPoint = temp[1];
+        playerHUD = PlayerHUD.Instance;
+        if (dropPoint == null)
+        {
+            Debug.LogError("dropPoint game object can not be found!");
+        }
+    }
+
+    public void OnEntry()
+    {
+        UpdateManager.Instance.AddUpdatable(this);
+
+    }
+
+    public void OnExit()
+    {
+        UpdateManager.Instance.RemoveUpdatable(this);
+        PlayerHUD.Instance.HideInteraction();
+    }
+   
+}
